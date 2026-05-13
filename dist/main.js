b3f8fca55d1b0024a820b30b8301df34d1afabf7
Valentin-Yordanov <vvbg123321@gmail.com>
2026-05-13 17:54:49 +0300

    The begining of everything.
    Created:
    Main pages that will be used
    Some styling
    Mostly preperatcions for the future

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,35 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// 1. Define the HTML for the Navbar
+const navbarHTML = `
+<nav class="navbar">
+    <div class="logo">Mona's Gallery</div>
+    <ul class="nav-links">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="info.html">Info</a></li>
+        <li><a href="catalog.html">All Paintings</a></li>
+    </ul>
+    <button id="lang-switch">EN / BG</button>
+</nav>
+`;
+// 2. Define the HTML for the Footer
+const footerHTML = `
+<footer class="footer">
+    <p>&copy; 2026 Mona's Gallery. All rights reserved.</p>
+    <p>Contact: info@monasgallery.com</p>
+</footer>
+`;
+// 3. Inject them into the page when it loads
+function loadComponents() {
+    const navContainer = document.getElementById('navbar-container');
+    const footerContainer = document.getElementById('footer-container');
+    if (navContainer) {
+        navContainer.innerHTML = navbarHTML;
+    }
+    if (footerContainer) {
+        footerContainer.innerHTML = footerHTML;
+    }
+}
+// Run the function
+loadComponents();
 //# sourceMappingURL=main.js.map
